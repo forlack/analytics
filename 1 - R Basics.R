fdd01e2910a94d36041a282f3e897572fe8470cd
chase thomas <chase thomas>
2017-03-09 15:32:33 -0700

    1

--- 1 - R Basics.R
+++ 1 - R Basics.R
@@ -7,3 +7,5 @@ sales = c(1420.29,2900.57,1900.43,1400.91)
 rating = c(8,9.8,6.1,8.6)
 
 table = data.frame(names,gender,years,sales,rating, stringsAsFactors = FALSE)
+summary(table$sales)
+mean(table$rating)
